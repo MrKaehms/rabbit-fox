9b977f17c2bfebf2b287e4bd0e2ba10b102de602
Kana Mishra <mishrakana@gmail.com>
2016-01-27 08:17:51 -0800

    Rabbit Final Version
    Using delays instead of number of jumps

--- rabbit.java
+++ rabbit.java
@@ -37,11 +37,11 @@ public class rabbit extends Actor
     private int turnAmount= 5; // you can set this between 5 and 20. Fixed for duration of game
                                // turnAmount must be a multiple of 5.
                    
-    private int maxJumps  = 4;  // Can be 1 to 5
+    private int maxJumps  = 50;  // Can be 1 to 5
     private int currJump =  0;  // counter to keep track of number of jumps before
                                 // rabbit goes into rest mode.
     
-    private int restCycle = 5; // This is the number of act cycles that
+    private int restCycle = 100; // This is the number of act cycles that
                                  // a rabbit must rest when he is in rest mode.
                                  // When in rest mode, a rabbit is completely still.
     private boolean inRestMode = false; // Set to true after maxJumps                            
@@ -56,11 +56,25 @@ public class rabbit extends Actor
         {
             move(smallJump);
             currJump++;
+            if (currJump >= maxJumps)
+            {
+                inRestMode = true;
+                Greenfoot.delay(restCycle);
+                inRestMode = false;
+                currJump = 0;
+            }
         }
         if (Greenfoot.isKeyDown("k"))
         {
             move(bigJump);
             currJump++;
+            if (currJump >= maxJumps)
+            {
+                inRestMode = true;
+                Greenfoot.delay(restCycle);
+                inRestMode = false;
+                currJump = 0;
+            }
         }
         if (Greenfoot.isKeyDown("o"))
         {
@@ -74,12 +88,6 @@ public class rabbit extends Actor
         {
             turn(180);
         }
-        if (currJump >= maxJumps)
-        {
-            inRestMode = true;
-            Greenfoot.delay(restCycle);
-            inRestMode = false;
-            currJump = 0;
-        }
+        
     }    
 }
